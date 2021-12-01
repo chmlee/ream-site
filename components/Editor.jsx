2bf5a12cbe6d63079e87c5f12874e4d18770a122
Chih-Ming Louis Lee <louis@chihminglee.com>
2021-12-01 12:03:22 -0600

    add output options

--- components/Editor.jsx
+++ components/Editor.jsx
@@ -3,48 +3,123 @@ import init, { ream2csv } from 'reamc';
 
 export default function Editor() {
 
-    const [ output, setOutput ] = useState([[]]);
+    const outputModes = [
+        { id: 'table', display: 'Table' },
+        { id: 'doc', display: 'Doc' },
+        { id: 'csv', display: 'CSV' },
+    ]
+    const [ outputMode, setOutputMode ] = useState(outputModes[0].id);
+    const [ result, setResult ] = useState([[]]);
+    const [ autoCompile, setAutoCompile ] = useState(true);
     const [ error, setError ] = useState('');
 
-    async function update(e) {
-        await init();
-        const new_input = e.target.value
-        const output = ream2csv(new_input)
-        if (Array.isArray(output)) {
-            setOutput(output)
-            setError("")
-        } else if (output !== null) {
-            const e = Object.keys(output).map(key => {
-                const msg = [ key, output[key] ].join(": ");
+    async function compile(input) {
+        await init()
+
+        const result = ream2csv(input)
+        console.log(result)
+        if (Array.isArray(result)) {
+            setError('no error')
+            return ream2csv(input)
+        } else if (result !== null) {
+            const e = Object.keys(result).map(key => {
+                const msg = [ key, result[key] ].join(": ")
                 return msg
-            })[0];
-            setError(e);
+            })[0]
+            setError(e)
         }
     }
 
+    async function updateInput(e) {
+        if (!autoCompile) return null
+
+        const input = e.target.value
+        const result = await compile(input)
+        setResult(result)
+    }
+
+    async function toggleCompile(e) {
+        const input = e.target.parentElement.querySelector("#input").value
+        const result = await compile(input)
+        setResult(result)
+    }
+
+    function updateOutputMode(e) {
+        outputMode = e.target.id;
+        setOutputMode(outputMode, outputMode)
+    }
+
+
+    function updateAutoCompile(e) {
+        setAutoCompile(!autoCompile)
+    }
+
+
+
     return (
         <div className="editor-container">
+            <div class="editor-tab">Input</div>
             <textarea 
                 className="input"
-                id="" 
+                id="input" 
                 name="" 
                 cols="30" 
                 rows="10"
-                onChange={update}
+                onChange={updateInput}
             />
-            <div className="error" >
+            <div className="output-mode-option-container">
+            {
+                outputModes.map(mode => {
+                    return (
+                        <div 
+                            className="editor-tab output-mode"
+                            id={mode.id}
+                            key={mode.id}
+                            onClick={updateOutputMode}
+                        >
+                        {mode.display}
+                        </div>
+                    )
+                })
+
+            }
+            </div>
+            <Output 
+                result={result}
+                outputMode={outputMode}
+            />
+            <button onClick={toggleCompile}>
+                Compile
+            </button>
+            <label className="autoCompileButton">
+                <input 
+                    type="checkbox" 
+                    onChange={updateAutoCompile}
+                    defaultChecked={autoCompile}
+                />
+                <span>Auto Update</span>
+            </label>
+            <div class="error-message">
                 {error}
             </div>
-            <Spreadsheet 
-                className="output"
-                array={output}
-            />
+        </div>
+    )
+
+}
+
+function Output({ result, outputMode }) {
+
+    //console.log(result, outputMode)
+
+    return (
+        <div className="output-container">
+            Output
         </div>
     )
 }
 
 function Spreadsheet({ array, style }) {
-    console.log(array)
+    //console.log(array)
     return (
         <table style={style}>
         <tbody>
