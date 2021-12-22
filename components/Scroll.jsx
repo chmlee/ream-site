e7ed023a18b135ddea9e6675c3493ff885284f24
Chih-Ming Louis Lee <louis@chihminglee.com>
2021-12-22 15:09:45 -0600

    make menu and editor responsive

--- components/Scroll.jsx
+++ components/Scroll.jsx
@@ -1,13 +1,39 @@
 import Link from 'next/link';
+import { useState, useEffect } from 'react';
 
 import { sections } from '/components/sections'
 
 import styles from './Scroll.module.scss';
 
 export default function Scroll() {
+
+
+    const [ menu, setMenu ] = useState(false)
+    const toggleMenu = () => {
+        if (window.screen.width < 600) {
+            const nav = document.querySelector('#nav-wrapper')
+            setMenu(!menu)
+            nav.style.display = menu ? 'block' : 'none'
+        }
+    }
+
+
+    useEffect(() => {
+        if (window.screen.width < 600) {
+            const nav = document.querySelector('#nav-wrapper')
+            nav.style.display = 'none'
+        }
+    }, [])
+
     return (
         <nav className={styles.nav}>
-            <Menu sections={sections} i={1} />
+            <div className={styles['menu-button']} id="menu-button" onClick={toggleMenu}>
+                menu
+            </div>
+            <div className={styles['nav-wrapper']} id="nav-wrapper"
+            >
+                <Menu sections={sections} i={1} />
+            </div>
         </nav>
     )
 }
